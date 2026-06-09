92d1c48f8923151f1622ff5cfde6ce66f622f047
jonnathangc66-lgtm <jonnathangc66@gmail.com>
2026-06-08 22:25:51 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,59 @@
+window.onload = function(){
+
+const cover = document.getElementById("cover");
+const invitation = document.getElementById("invitation");
+const music = document.getElementById("music");
+const openBtn = document.getElementById("enterInvitation");
+
+if(!openBtn){
+alert("No se encontró el botón enterInvitation");
+return;
+}
+
+openBtn.onclick = function(){
+
+music.volume = 0.5;
+
+music.play().catch(()=>{});
+
+cover.style.display = "none";
+
+invitation.style.display = "block";
+
+updateCountdown();
+
+};
+
+const weddingDate =
+new Date("November 14, 2026 18:00:00").getTime();
+
+function updateCountdown(){
+
+const now = new Date().getTime();
+
+const distance = weddingDate - now;
+
+const days =
+Math.floor(distance / (1000*60*60*24));
+
+const hours =
+Math.floor((distance%(1000*60*60*24))/(1000*60*60));
+
+const minutes =
+Math.floor((distance%(1000*60*60))/(1000*60));
+
+const seconds =
+Math.floor((distance%(1000*60))/1000);
+
+document.getElementById("days").innerHTML = days;
+document.getElementById("hours").innerHTML = hours;
+document.getElementById("minutes").innerHTML = minutes;
+document.getElementById("seconds").innerHTML = seconds;
+
+}
+
+setInterval(updateCountdown,1000);
+
+updateCountdown();
+
+};
